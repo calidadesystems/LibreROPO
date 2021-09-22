afe8e73db90d5ce92bcacea327e20416d60bb63e
Miguel Lorenzo Amarelle <miguel@calidadesystems.com>
2021-09-22 22:14:36 +0200

    most important commit ever

--- LibreROPO/Form1.Designer.cs
+++ LibreROPO/Form1.Designer.cs
@@ -29,10 +29,17 @@ namespace LibreROPO
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(699, 296);
+            this.Name = "Form1";
+            this.Text = "LibreROPO";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
