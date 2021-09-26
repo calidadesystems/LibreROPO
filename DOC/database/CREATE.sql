a0dcc9422e46d764623de3ba5cfee1f75902f9fd
Miguel Lorenzo Amarelle <miguel@calidadesystems.com>
2021-09-27 00:58:11 +0200

    mkore data

--- DOC/database/CREATE.sql
+++ DOC/database/CREATE.sql
@@ -272,3 +272,96 @@ insert into PAIS(identificador,pais)
 		('ZM','Zambia'),
 		('ZW','Zimbabwe')
 		;
+		
+CREATE TABLE PROVINCIA(
+	INE INTEGER,
+	PROVINCIA TEXT(200)
+);
+
+insert into PROVINCIA(PROVINCIA,INE) 
+	values
+		('Álava',01),
+		('Albacete',02),
+		('Alicante',03),
+		('Almería',04),
+		('Ávila',05),
+		('Badajoz',06),
+		('Baleares',07),
+		('Barcelona',08),
+		('Burgos',09),
+		('Cáceres',10),
+		('Cádiz',11),
+		('Castellón',12),
+		('Ciudad Real',13),
+		('Córdoba',14),
+		('La Coruña',15),
+		('Cuenca',16),
+		('Girona',17),
+		('Granada',18),
+		('Guadalajara',19),
+		('Guipúzcoa',20),
+		('Huelva',21),
+		('Huesca',22),
+		('Jaén',23),
+		('León',24),
+		('Lleida',25),
+		('La Rioja',26),
+		('Lugo',27),
+		('Madrid',28),
+		('Málaga',29),
+		('Murcia',30),
+		('Navarra',31),
+		('Orense',32),
+		('Asturias',33),
+		('Palencia',34),
+		('Las Palmas',35),
+		('Pontevedra',36),
+		('Salamanca',37),
+		('Tenerife',38),
+		('Cantabria',39),
+		('Segovia',40),
+		('Sevilla',41),
+		('Soria',42),
+		('Tarragona',43),
+		('Teruel',44),
+		('Toledo',45),
+		('Valencia',46),
+		('Valladolid',47),
+		('Bizcaia',48),
+		('Zamora',49),
+		('Zaragoza',50),
+		('Ceuta',51),
+		('Melilla',52)
+	;
+
+
+	
+create table RESPONSABLE(
+	NIFResponsable TEXT(9),
+	ROPOResponsable TEXT(20)
+);
+
+create table CLIENTE(
+	NIFDestino TEXT(20),
+	ROPODestino TEXT(20),
+	EntidadDestino TEXT(200),
+	CorreoElectronicoDestino TEXT(255),
+	TelefonoDestino	TEXT(10),
+	FaxDestino		TEXT(20),
+	DireccionDestino TEXT(255),
+	CodPostalDestino TEXT(5),
+	PaisDestino	TEXT(2),
+	ProvinciaDestino	TEXT(2)
+);
+
+
+		
+create table REGISTRO(
+	TIPO INTEGER,
+	fecha TEXT,
+	NIFResponsable TEXT(9),
+	CODIGOOPERACION INTEGER,
+	
+);
+
+
