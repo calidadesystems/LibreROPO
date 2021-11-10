2b3a8212eba32e3fd12cce8d058d44b5d448c581
Miguel Lorenzo Amarelle <miguel@calidadesystems.com>
2021-11-10 11:36:55 +0100

    añadida clase unidad

---
+++ LibreROPO/Unidad.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreROPO
+{
+    class Unidad
+    {
+        private int codigo;
+        private string descripcion;
+
+        public Unidad(int codigo, string descripcion) 
+        {
+            this.Codigo = codigo;
+            this.descripcion = descripcion;
+        }
+
+        public int Codigo { get => codigo; set => codigo = value; }
+
+    }
+}
